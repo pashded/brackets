9be4322e7ea0d805ef388e6e77281f19248591f6
pashded <7acybnwush@gmail.com>
2019-03-24 19:49:22 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ let arr = [];
+  arr.push(str[0]);
+  for (let i=1; i<str.length; i++) {
+    arr.push(str[i]);
+    for (let j=0; j<bracketsConfig.length; j++) {
+      if (JSON.stringify([arr[arr.length - 2], arr[arr.length - 1]]) === JSON.stringify(bracketsConfig[j])) {
+        arr.pop();
+        arr.pop();
+      }
+    }
+  }
+  return arr.length == 0 ? true : false;    
 }
